eb0b3aa0ba08b9ebc194ef4471d0c20b0ceb013a
Jerome Haltom <wasabi@cogito.cx>
2013-09-08 20:05:16 -0500

    Nifty.

--- Nancy.Bootstrappers.Mef.Tests/NancyTypeCatalogTests.cs
+++ Nancy.Bootstrappers.Mef.Tests/NancyTypeCatalogTests.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.Composition.Hosting;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Nancy.ViewEngines;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Nancy.Bootstrappers.Mef.Tests
 {
@@ -14,15 +11,6 @@ namespace Nancy.Bootstrappers.Mef.Tests
         {
             return new NancyTypeCatalog(typeof(Nancy.NancyEngine).Assembly.GetTypes());
         }
-
-        //[TestMethod]
-        //public void ViewLocationProviderTest()
-        //{
-        //    var a = new NancyTypeCatalog(typeof(IViewLocationProvider), typeof(ResourceViewLocationProvider));
-        //    var c = new CompositionContainer(a);
-        //    var i = c.GetExports<IViewLocationProvider>();
-        //    Assert.IsTrue(i.Count() == 1);
-        //}
 
     }
 
